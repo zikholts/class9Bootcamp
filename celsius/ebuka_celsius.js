96fe32471b9d8d94f8bae1efc2e5f9123676bbb6
Ebuka <victorohaeche@gmail.com>
2015-06-10 17:54:39 +0100

    edited ebuka_celsius.js

--- celsius/ebuka_celsius.js
+++ celsius/ebuka_celsius.js
@@ -1,13 +1,14 @@
 function convertToCelsius(temp) {
     var f = ((9 / 5) * temp) + 32;
-    console.log(f);
-    
+    return f;
 }
 
 function convertManyCelsius(temp) {
    var f = [];
    for(var i = 0; i < temp.length; i++){
        f.push(convertToCelsius(temp[i]));
-   }
+    }
+    return f;
 }
+
 convertManyCelsius([1, 25, 60, 100]);
