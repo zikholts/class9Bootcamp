1fbacb9800a25aabc24087a7b8aadf2a7a647616
Jekoyemi Funminiyi <drjekoyemi@gmail.com>
2015-06-10 18:31:57 +0100

    perfected the branch

--- celsius/eche_convertCelsius.js
+++
@@ -1,9 +0,0 @@
-var fahrenheit = function(celsuis){
-    var celarray = [];
-    for (var i=0; i<=celsuis.length - 1; i++) {
-        fahMeth = ((9/5)*celsuis[i]) + 32;
-        celarray.push(fahMeth);
-    }
-    return celarray;
-}
-console.log(fahrenheit([5,3,5]));
