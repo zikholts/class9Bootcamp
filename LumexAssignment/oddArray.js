26b67168741d795d2c35e00ab6edd5979c69a49c
Ogundele Olumide <olumideralph@gmail.com>
2015-06-11 15:21:09 +0100

    My 3 Assignments

---
+++ LumexAssignment/oddArray.js
@@ -0,0 +1,11 @@
+function oddArray(array) {
+       var output = [];
+        for(var i=0; i < array.length; i++){
+            array[i];
+        if(array[i] % 2 == 1) {
+           output.push(array[i]);   
+           } 
+        }
+        return output;
+}
+oddArray([1, 2, 5, 7]);
